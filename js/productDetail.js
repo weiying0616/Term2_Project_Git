05be5d13b5f37621bf13912db50ae23a3803655c
weiying0616 <977390393@qq.com>
2020-12-08 09:13:23 +0800

    第九章交互效果作业

---
+++ js/productDetail.js
@@ -0,0 +1,16 @@
+/*
+产品
+魏滢
+2020.12.7
+*/
+$(function(){
+    magnifier({
+		magnifier : "#magnifier1",//最外层的大容器
+		width : 340,//承载容器宽
+		height : 470,//承载容器高
+		moveWidth : null,//如果设置了移动盒子的宽度，则不计算缩放比例
+		zoom : 5//缩放比
+	
+	
+	})
+})
--- js/shopcart.js
+++ js/shopcart.js
@@ -4,8 +4,8 @@
 魏滢
 */
 //定义三个变量
-
 $(function(){
+
 
 var $theadInput = $('table thead input[type=checkbox]'); //头部选择框
 var $bodyInput = $('table tbody input[type=checkbox]');//中间选择框
@@ -17,6 +17,9 @@ $theadInput.change(function(){
 
     $bodyInput.prop('checked',state);
     $allPriceInput.prop('checked',state);
+
+    //调用计算总价函数
+    calcTotalPrice();
 })
 
 $allPriceInput.change(function(){
@@ -24,26 +27,30 @@ $allPriceInput.change(function(){
 
     $bodyInput.prop('checked',state);
     $theadInput.prop('checked',state);
+
+    //调用计算总价函数
+    calcTotalPrice();
 })
 
 $bodyInput.change(function(){
+    //顶一个标杆
     var flag = true;
+    //总价
+    
 
-    var totalPrice = 0;
-
+    //循环表格中所有选择框的的选中状态
     $bodyInput.each(function(i,input){
         if (!$(input).prop('checked')){
             flag = false;
-        }else{
-            totalPrice += parseFloat ($(this).closest('tr').find('.subprice').text());
         }
     })
 
     $theadInput.prop('checked',flag)
     $allPriceInput.prop('checked',flag)
 
-    //渲染到总价对应的位置
-    $('.total').text(totalPrice.toFixed(2))
+    //调用计算总价函数
+    calcTotalPrice();
+
 })
 
 //数量的加减功能
@@ -58,6 +65,9 @@ $('.add').on('click',function(){
     $nextInput.val(oldVal);
 
     subTotalPrice(oldVal,$(this)); 
+
+    //调用计算总价函数
+    calcTotalPrice();
 })
 
 //减
@@ -72,6 +82,9 @@ $('.reduce').on('click',function(){
     $prevInput.val(oldVal);
 
     subTotalPrice(oldVal,$(this));    
+
+    //调用计算总价函数
+    calcTotalPrice();
 })
 
 //抽取一个小计的函数
@@ -83,15 +96,55 @@ $('.reduce').on('click',function(){
 //删除
 $('.del').click(function(){
     $(this).closest('tr').remove();
+    calcGoodsCount();
 })
 
-//计算总价
-//$bodyInput.each(function(i,input){
-  //  var totalPrice = 0;
 
-    //if ($(input).prop('checked')){
-      //  totalPrice += parseFloat ($(this).closest('tr').find('.subprice').text());
-    //}
-//})
+ //计算总价的函数
+ function calcTotalPrice(){
+     var count = 0;
+
+     //定义变量 保持总价格
+     var totalPrice = 0;
+
+     console.log(totalPrice)
+
+     $('table tbody input[type=checkbox]').each(function(i,input){
+         if($(input).prop('checked')){
+             count ++
+             //累加价格
+             totalPrice += parseFloat ( $(input).closest('tr').find('.subprice').text())
+         }
+     })
+
+     console.log(totalPrice)
+
+     //把总价渲染到对应位置
+     $('.total').text( totalPrice.toFixed(2) )
+
+     $('.count').text(count)
+
+ }
+
+
+ //全部商品
+ function calcGoodsCount(){
+      $('.goodsCount').text($('table tbody tr').length)
+ }
+ calcGoodsCount();
+//删除选中商品
+$('.deleteChecked').on('click',function(){
+    $bodyInput.each(function (i,input){
+        if($(this).prop('checked')){
+            $(this).closest('tr').remove();
+        }
+    })
+
+    //计算总价
+    calcTotalPrice();
+    //计算商品数量
+    calcGoodsCount();
+})
+
 
 })
